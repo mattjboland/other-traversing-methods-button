8f6a5ebc56dbe4052238b3057a96493646660ff9
mattjboland <mattjamesboland@gmail.com>
2020-02-05 21:07:23 +0000

    added to script to make changes to buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
  // put your code here
+
+/* $(".theButton").click(function(){
+     $("#panel .container").siblings().hide();
+ })*/
+
+ /*$(".theButton").click(function(){
+     $(this).hide();
+ })*/
+
+ $(".theButton").click(function(){
+     $("#panel .container").siblings().fadeTo("slow", 0.5);
+ })
+
+ $(".superButton").click(function(){
+     $("#panel .container").siblings().fadeTo("fast", 1);
+ })
 });
